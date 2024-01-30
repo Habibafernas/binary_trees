728615f3496ba394da575b6a444f8cccf67b91cd
Habibafernas <habibafernas@gmail.com>
2024-01-31 01:05:23 +0200

    A

--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -24,12 +25,15 @@ struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 };
-
-void binary_tree_print(const binary_tree_t *);
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+
+/* binary_tree_print.c */
+void binary_tree_print(const binary_tree_t *);
+
+/* tasks */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
