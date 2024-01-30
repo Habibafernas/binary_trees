16dec74f7c970d3974e7b50d29ae266855735e09
Habibafernas <habibafernas@gmail.com>
2024-01-31 00:42:52 +0200

    A

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 #include <stddef.h>
 #include <stdlib.h>
@@ -19,7 +19,7 @@ struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
 
 void binary_tree_print(const binary_tree_t *);
 
